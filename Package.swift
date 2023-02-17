ddb86f16ffaa784090fad3dcd6e4e411ba0bf1b4
Build Er <builder@macmini6.local>
2023-02-16 23:51:42 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXFlexLayout",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXFlexLayout",
+			targets: ["GXFlexLayoutWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXFlexLayoutWrapper",
+				dependencies: [
+					"GXFlexLayout",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXFlexLayout",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "a2f7baf28c48e4ebbfd1c1819b773074f74c3eb66edb04b023bdb5245d422704"
+		)
+	]
+)
