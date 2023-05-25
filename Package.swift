b321f51a684a04747bcceac14b816c5cca9441e6
Build Er <builder@macmini6.local>
2023-05-25 11:01:26 -0300

    Package version: 1.0.0-beta+20230525135158

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFlexLayout",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-1.0.0-beta+20230525122919.xcframework.zip",
-			checksum: "496bc43c48b252db8f57e200186ec8ade1c3ee2770e7fd8aaafc9d402c26af58"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-1.0.0-beta+20230525135158.xcframework.zip",
+			checksum: "594357fb4e929b7458230e6e2e2cadca72b87d48c4aea8d850b2cd2ba1482bb5"
 		)
 	]
 )
