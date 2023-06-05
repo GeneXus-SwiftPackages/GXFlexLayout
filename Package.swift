4298fb2fc704d2876b5b6a7dd7036e5cb0f9bdf3
genexusbot <builder@genexus.com>
2023-06-05 13:30:23 -0300

    Package version: 1.0.0-beta+20230605162159

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFlexLayout",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-1.0.0-beta+20230601044120.xcframework.zip",
-			checksum: "4fa49b5824eee36f2c3e8ab773fd78f14506a245530e1b455b3e650179902697"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-1.0.0-beta+20230605162159.xcframework.zip",
+			checksum: "9917aae08680adff0e4a6172427a71b5c33af1734c3f96f6f958ad114889b248"
 		)
 	]
 )
