2cf37b0fa037b660c0225518a45a47e5279e7b87
The iOS Team @ GeneXus <builder@genexus.com>
2025-12-09 07:33:01 -0500

    Package version: 4.1.0-beta.14

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXFlexLayoutWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.13"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "4.1.0-beta.13")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.14"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "4.1.0-beta.14")
 	],
 	targets: [
 		.target(name: "GXFlexLayoutWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFlexLayout",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-4.1.0-beta.13.xcframework.zip",
-			checksum: "683ece6df58ac67748505987a594c121d2ce1b7c9287fe4502e70feb52aa09a8"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFlexLayout-4.1.0-beta.14.xcframework.zip",
+			checksum: "7db6fe14552f011b2f3ba1a9e5b0b9de302eee24cdae8a9fa2037974fffce027"
 		)
 	]
 )
